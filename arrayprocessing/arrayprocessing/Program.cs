01ca8275651c773554ec0b59997d541d10cc105f
Ravinder Singh <696464@student.tor.loc>
2019-04-18 14:32:38 -0400

    inputarray code

--- arrayprocessing/arrayprocessing/Program.cs
+++ arrayprocessing/arrayprocessing/Program.cs
@@ -8,6 +8,36 @@ namespace arrayprocessing
 {
     class Program
     {
+        public int[] inputArray = new int[10] { 3, 11, 9, 11, 4, 11, 5, 9, 2, 1 };
+        public int[,] frequencyOfOccuranceArray = new int[10, 2];
+
+        static void Main(string[] args) { new Program().Run(); }
+
+        public void Run() { this.InterateOverArray(); }
+       
+        public void InterateArray()
+        {
+            int output;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                CountFrequencyOfOccurance(inputArray[i]);
+
+            }
+        }
+
+        public void CountFrequencyOfOccurance(int inputValue)
+        {
+
+            //get a handle on input array
+            //find out if inputvalue is in INPUT AARRAY
+            for(int i = 0; i < inputArray.Length; i++)
+            {
+                if(inputArray[i] == inputValue)
+                {
+                    
+                }
+            }
+        }
         static void Main(string[] args)
         {
 
