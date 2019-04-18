07e49f481ad94844b9f3d891542fb3ac559f2d19
Ravinder Singh <696464@student.tor.loc>
2019-04-18 14:00:35 -0400

    working code longest word

--- arrayprocessing/arrayprocessing/Program.cs
+++ arrayprocessing/arrayprocessing/Program.cs
@@ -4,44 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArrayProccesing
+namespace arrayprocessing
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Program a = new Program();
-            Console.WriteLine("the longest word is:");
+
+            Console.WriteLine("the longest word is : ");
+
             Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+
+            Console.ReadLine();
         }
 
-        public MyFunctionA(string input)
-        { //Write a c# program to find the longest word ina string.
-            string[] words = input.Split('');
+        public String MyFunctionA(string input)
+        {
+            string[] words = input.Split(' ');
 
             int wordArrayLength = words.Length;
+
             int[] wordsLength = new int[wordArrayLength];
-            int x = 0;
 
             foreach (var word in words)
             {
-                //TODO
+
             }
-            //loop post condition: we now have array wordsLength which
-            //contains the lengths of each word
 
             string currentWord = words[0];
+
             string nextWord;
-            string longestWord = currentWord;
 
             for (int y = 0; y < words.Length - 1; y++)
             {
                 currentWord = words[y];
+
                 nextWord = words[y + 1];
 
             }
+
+            return currentWord;
         }
 
     }
 }
-
